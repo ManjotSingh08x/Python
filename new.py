75d89df41393fc335418f398cbd6a4acdaa83c81
Manjot Singh <mithugunnu2006@gmail.com>
2024-05-14 09:35:32 -0700

    this is fine

---
+++ new.py
@@ -0,0 +1,12 @@
+print("hello world")
+a = "something"
+if a == "something":
+    print(a)
+
+
+def functionacalojdf(input):
+    if input:
+        print(input)
+
+answer = input("enter a number")
+functionacalojdf(answer)
