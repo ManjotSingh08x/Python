ff401aa14d4d13940f24ac4bca1c1095b9a9f5e9
Manjot Singh <mithugunnu2006@gmail.com>
2024-05-27 08:45:15 -0700

    no

---
+++ vscode/guessing_game.py
@@ -0,0 +1,24 @@
+import random 
+def display_game(number_of_attempts):
+	answer = random.randint(0,100)
+	print("lets play a game \n")
+	print(f"you are given {number_of_attempts} tries")
+	life_index = 10 
+	while life_index > 0:
+		attempt = int(input("guess the number (between 1 to 100): "))
+		if attempt < answer:
+			life_index -= 1
+			print("too low try again")
+
+		elif attempt > answer:
+			life_index -= 1
+			print("too high try again")
+
+		else:
+			print("congrats you guessed the number!! it was ", answer)
+			return 1
+	return 0
+
+display_game(10)
+
+
