454178227d5e8bf623f26e4ba105397007b584db
Manjot Singh <mithugunnu2006@gmail.com>
2024-05-29 05:27:24 -0700

    ffajlskdfjalkdfj

--- vscode/c4_pizzas.py
+++ vscode/c4_pizzas.py
@@ -1,5 +1,13 @@
-pizzas = ["peperonni", 'cheese', 'farmhouse', 'chicken', 'pineapple']
+pizzas = ["peperonni", 'cheese', 'farmhouse', 'chicken', 'pineapple', "spicy", 'special']
 for pizza in pizzas:
     print(f"I like {pizza} pizza")
     
 print("I really like all kinds of pizzas")
+print("the first three items are: ")
+print(pizzas[0:3])
+print("the middle three items are: ")
+print(pizzas[2:5])
+print('the last three items are:')
+print(pizzas[-3:])
+
+friend_pizzas = pizzas[:]
