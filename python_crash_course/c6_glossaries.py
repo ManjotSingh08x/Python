d0f67d214e89e41fc018c75202537de56d71b529
Manjot Singh <mithugunnu2006@gmail.com>
2024-05-31 23:49:19 -0700

    done part 1 of the excersice

--- python_crash_course/c6_glossaries.py
+++ python_crash_course/c6_glossaries.py
@@ -5,3 +5,20 @@ glossary = {'print': 'prints the output of the program into the terminal',
 
 for key, value in glossary.items():
     print(f'* The meaning of {key}: \n  {value}')
+
+rivers = {'nile': 'egypt',
+          'krishna': 'india',
+          'yellow river': 'china',
+          'missisipi': 'united states of america',
+          'amazon': 'brazil',}
+
+for river, country in rivers.items():
+    print(f'The {river.title()} runs through {country.title()}')
+
+print('Here is the list of the rivers:')    
+for river in rivers.keys():
+    print("* " + river.title())
+
+print('Here is the list of the countries:')
+for country in rivers.values():
+    print("* " + country.title())
