5a6eb00f0cb1a4319d4a566fdd630043df2524ac
Manjot Singh <mithugunnu2006@gmail.com>
2024-05-31 23:34:11 -0700

    removed errors

--- python_crash_course/c6_glossaries.py
+++ python_crash_course/c6_glossaries.py
@@ -1,7 +1,7 @@
 glossary = {'print': 'prints the output of the program into the terminal',
             'def': 'used to define a function in python',
             'lists': 'provides the array data structure and is used to store data in linear form',
-            'lower()': 'returns a copy of the strin with capital letters converted to lowercase',}
+            'lower()': 'returns a copy of the string with capital letters converted to lowercase',}
 
 for key, value in glossary.items():
-    print(f'* The meaning of {key}: \n\t{value}')
+    print(f'* The meaning of {key}: \n  {value}')
