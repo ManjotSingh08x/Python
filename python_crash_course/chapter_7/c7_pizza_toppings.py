e64cba6f5a417f31ce9511352e5de08402eb0652
Manjot Singh <mithugunnu2006@gmail.com>
2024-06-01 09:06:06 -0700

    creating different versions in tickets

--- python_crash_course/chapter_7/c7_pizza_toppings.py
+++ python_crash_course/chapter_7/c7_pizza_toppings.py
@@ -1,16 +1,6 @@
 prompt = '\nEnter a pizza topping that you want'
 prompt += '\n(type "quit" to exit the program): '
 
-#active test version
-# active = True
-# while active:
-#     toppings = input(prompt)
-#     if toppings == 'quit':
-#         active = False
-#         print('\nYour order was finalized')
-#     else:
-#         print(f'I will add {toppings} to your pizza')
-
 #conditional test version
 # toppings = ''
 # while toppings != 'quit':
@@ -21,9 +11,21 @@ prompt += '\n(type "quit" to exit the program): '
 #     else:
 #         print(f'\nYour order was finalized')
 
+#active test version
+# active = True
+# while active:
+#     toppings = input(prompt)
+
+#     if toppings == 'quit':
+#         active = False
+#         print('\nYour order was finalized')
+#     else:
+#         print(f'I will add {toppings} to your pizza')
+
 #break version
 while True:
     toppings = input(prompt)
+    
     if toppings == 'quit':
         print(f'\nYour order was finalized')
         break
