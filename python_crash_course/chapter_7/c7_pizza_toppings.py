adda9a2985d49e083998dd85129571acbc354af0
Manjot Singh <mithugunnu2006@gmail.com>
2024-06-01 08:25:05 -0700

    soling chapter 7

--- python_crash_course/chapter_7/c7_pizza_toppings.py
+++ python_crash_course/chapter_7/c7_pizza_toppings.py
@@ -1,11 +1,34 @@
 prompt = '\nEnter a pizza topping that you want'
 prompt += '\n(type "quit" to exit the program): '
 
-active = True
-while active:
+#active test version
+# active = True
+# while active:
+#     toppings = input(prompt)
+#     if toppings == 'quit':
+#         active = False
+#         print('\nYour order was finalized')
+#     else:
+#         print(f'I will add {toppings} to your pizza')
+
+#conditional test version
+# toppings = ''
+# while toppings != 'quit':
+#     toppings = input(prompt)
+    
+#     if toppings != 'quit':
+#         print(f'I will add {toppings} to your pizza')
+#     else:
+#         print(f'\nYour order was finalized')
+
+#break version
+while True:
     toppings = input(prompt)
     if toppings == 'quit':
-        active = False
-        print('\nYour order was finalized')
+        print(f'\nYour order was finalized')
+        break
     else:
         print(f'I will add {toppings} to your pizza')
+        
+        
+    
