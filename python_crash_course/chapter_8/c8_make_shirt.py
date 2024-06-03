dd1e010b8bcd69800f3164c0048b629b6efc001d
Manjot Singh <mithugunnu2006@gmail.com>
2024-06-03 09:23:09 -0700

    dd

--- python_crash_course/chapter_8/c8_make_shirt.py
+++ python_crash_course/chapter_8/c8_make_shirt.py
@@ -1,5 +1,11 @@
-def make_shirt(size, message):
+def make_shirt(size='large', message='I love Python'):
     print(f'The size of shirt is {size} and the message printed is:\n{message}')
     
-make_shirt(30, 'Python is op')
-make_shirt(message='python is too op', size=66)
+make_shirt('small', 'Python is op')
+make_shirt(message='python is too op', size='large')
+make_shirt()
+
+def describe_city(city, country="India"):
+    print(f'{city.title()} is in {country.title()}')
+    
+describe_city('Delhi')
