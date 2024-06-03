d438e30ec9528d4cc50c88d19d7f438c4d70677e
Manjot Singh <mithugunnu2006@gmail.com>
2024-06-03 08:28:27 -0700

    started chapter 8

---
+++ python_crash_course/chapter_8/c8_make_shirt.py
@@ -0,0 +1,5 @@
+def make_shirt(size, message):
+    print(f'The size of shirt is {size} and the message printed is:\n{message}')
+    
+make_shirt(30, 'Python is op')
+make_shirt(message='python is too op', size=66)
