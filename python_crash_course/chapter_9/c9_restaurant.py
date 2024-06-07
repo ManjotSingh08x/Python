95f3430e0db2236811a54ae43a661c3e49ae6e98
Manjot Singh <mithugunnu2006@gmail.com>
2024-06-07 00:48:24 -0700

    updates c9_restaruant.py

--- python_crash_course/chapter_9/c9_restaurant.py
+++ python_crash_course/chapter_9/c9_restaurant.py
@@ -1,7 +1,8 @@
 class Restaurant:
-    def __init__(self, restaurant_name, cuisine_type):
+    def __init__(self, restaurant_name, cuisine_type, number_served=0):
         self.restaurant_name = restaurant_name
         self.cuisine_type = cuisine_type
+        self.number_served = number_served
     
     def describe_restaurant(self):
         print(f"Our restaurant's name is {self.restaurant_name}.")
@@ -9,6 +10,22 @@ class Restaurant:
         
     def open_restaurant(self):
         print('The restaurant is now open.')
+        
+    def get_number_served(self):
+        print(f"Our restaurant has served {self.number_served} customers.")
+    
+    def set_number_served(self, number):
+        if number >= self.number_served:
+            self.number_served = number
+        else:
+            print("decreasing number of served customers is not possible")
+        
+    def increment_number_served(self, number):
+        if number >= 0:
+            self.number_served += number
+        else:
+            print("decreasing number of served customers is not possible")
+        
         
 rest1 = Restaurant("Delhi Dhaba", 'Indian')
 rest2 = Restaurant("Annapurna", 'Chinese')
@@ -21,3 +38,13 @@ rest3.describe_restaurant()
 print(rest1.restaurant_name)
 print(rest1.cuisine_type)
 rest1.open_restaurant()
+
+rest1.get_number_served()
+
+rest1.set_number_served(500)
+rest1.get_number_served()
+
+rest1.increment_number_served(150)
+rest1.get_number_served()
+
+
