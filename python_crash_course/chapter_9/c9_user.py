4ba0a35a129e8f671ea282a69a0340e3d8591ee8
Manjot Singh <mithugunnu2006@gmail.com>
2024-06-07 01:50:36 -0700

    updates c9_user.py

--- python_crash_course/chapter_9/c9_user.py
+++ python_crash_course/chapter_9/c9_user.py
@@ -1,9 +1,10 @@
 class User:
-    def __init__(self, first_name, last_name, age, gender) -> None:
+    def __init__(self, first_name, last_name, age, gender, login_attempts=0) -> None:
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
         self.gender = gender
+        self.login_attempts = login_attempts
     
     def describe_user(self):
         print("Here is a summary of the profile:")
@@ -16,7 +17,17 @@ class User:
             print(f"Welcome, Mr. {self.first_name} {self.last_name}")
         else:
             print(f"Welcome, Mrs. {self.first_name} {self.last_name}")
-
+    
+    def increment_login_attempts(self, increment=1):
+        self.login_attempts += increment
+    
+    def reset_login_attempts(self):
+        self.login_attempts = 0 
+    
+    def display_login_attempts(self):
+        print(f"you have tried to login {self.login_attempts} times.")
+    
+    
 user1 = User("Mohit", "Kumar", 30, "Male")
 user2 = User("Harry", "Singh", 19, "Male")
 user3 = User("Samriti", "Rani", 22, "Female")
@@ -28,3 +39,10 @@ user3.describe_user()
 user1.greet_user()
 user2.greet_user()
 user3.greet_user()
+
+user1.increment_login_attempts()
+user1.display_login_attempts()
+user1.increment_login_attempts(4)
+user1.display_login_attempts()
+user1.reset_login_attempts()
+user1.display_login_attempts()
