5d27a5079b2041a1c27f483d80462c190104b986
Manjot Singh <mithugunnu2006@gmail.com>
2024-06-09 09:16:56 -0700

    creating a user interface

---
+++ python_crash_course/chapter_10/c10_remember_me.py
@@ -0,0 +1,38 @@
+from pathlib import Path
+import json 
+
+def get_new_user(path):
+    """Prompts for new user info"""
+    username = input("What is your name? ")
+    age = int(input("what is your age? "))
+    gender = input("what is your gender? ")
+    user_info = {'username': username, 'age': age, 'gender': gender}
+    contents = json.dumps(user_info)
+    path.write_text(contents)
+    print(f"we'll remember you when you come back, {username}!")
+
+def greet_user(path):
+    """greets the user using info stored in json"""
+    
+    user_info = get_stored_user_info(path)
+    if user_info:
+        print(f'Welcome back, {user_info['username']}!')
+        print(f'Your age is {user_info['age']} and your gender is {user_info['gender']}')
+    else: 
+        user_info = get_new_user(path)
+
+def get_stored_user_info(path):
+    "Retrieves user info from json file"
+    if path.exists():
+        contents = path.read_text()
+        user_info = json.loads(contents)
+        return user_info
+    else:
+        return None
+
+path = Path('chapter_10/username.json')
+
+if __name__ == '__main__':
+    greet_user(path)
+
+    
