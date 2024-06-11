56f68d9c3789a38315ee5fabf1adb6fb56a2f890
Manjot Singh <mithugunnu2006@gmail.com>
2024-06-11 07:16:50 -0700

    learning cpp operations

--- CPP/Day_1/datatypes.cpp
+++ CPP/Day_1/datatypes.cpp
@@ -2,9 +2,19 @@
 using namespace std;
 
 int main(){
-    int a = 5;
-    cout << "Value of a is " << a << " & size of a is " << sizeof(a) << endl;
+    int number = 5;
+    cout << "Value of a is " << number << " & size of a is " << sizeof(number) << endl;
     char ch = 'a';
     cout << "Value of ch is " << ch << " & size of ch is " << sizeof(ch) << endl;
+    // Learning operations
+    int a = 5;
+    int b = 2;
+    cout << "a + b = " << a + b << endl;
+    cout << "a - b = " << a - b << endl;
+    cout << "a * b = " << a * b << endl;
+    // Take special care in division
+    cout << "a / b = " << a / b << endl;
+    cout << "a / b (in float) " << (float)a/b << endl;
+    cout << "a % b = " << a % b << endl;
     return 1;
 }
