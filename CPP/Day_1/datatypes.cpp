8309b185ed9119fcc27fca7d0588b3219d8b46ad
Manjot Singh <mithugunnu2006@gmail.com>
2024-06-11 06:58:04 -0700

    learning datatypes

---
+++ CPP/Day_1/datatypes.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a = 5;
+    cout << "Value of a is " << a << " & size of a is " << sizeof(a) << endl;
+    char ch = 'a';
+    cout << "Value of ch is " << ch << " & size of ch is " << sizeof(a) << endl;
+    return 1;
+}
