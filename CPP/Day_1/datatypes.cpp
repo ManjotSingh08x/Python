b375dc4d3903739f44ec10b79c6a6ad82df3aa1c
Manjot Singh <mithugunnu2006@gmail.com>
2024-06-11 06:58:39 -0700

    nothing

--- CPP/Day_1/datatypes.cpp
+++ CPP/Day_1/datatypes.cpp
@@ -5,6 +5,6 @@ int main(){
     int a = 5;
     cout << "Value of a is " << a << " & size of a is " << sizeof(a) << endl;
     char ch = 'a';
-    cout << "Value of ch is " << ch << " & size of ch is " << sizeof(a) << endl;
+    cout << "Value of ch is " << ch << " & size of ch is " << sizeof(ch) << endl;
     return 1;
 }
