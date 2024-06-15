707666bd8e6aecf2766e96067a3065b390458b5a
Manjot Singh <mithugunnu2006@gmail.com>
2024-06-15 10:53:30 -0700

    learning about stadnard input output functionality in cpp

---
+++ CPP/LearnCPP/chapter1/lesson1,5q2.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+// Asks the user to provide three numbers and display them in a formatted output
+// int main(){
+//     std::cout << "Enter three numbers seperated by space: ";
+
+//     int x{};
+//     int y{};
+//     int z{};
+//     std::cin >> x >> y >> z;
+
+//     std::cout << "You entered " << x << ", " << y << ", and " << z << '.';
+
+//     return 0;
+// }
+
+
+
+int main(){
+	int a {};
+	double b {};
+
+	std::cin >> a >> b;
+
+	std::cout << "a="<< a << " b="<< b;
+	return 0;
+}
