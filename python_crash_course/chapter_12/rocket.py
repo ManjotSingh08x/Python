75ca14076e70043576b6d68ef291c59cc381edcb
Manjot Singh <mithugunnu2006@gmail.com>
2024-06-18 08:45:39 -0700

    creating a rocket

---
+++ python_crash_course/chapter_12/rocket.py
@@ -0,0 +1,12 @@
+
+
+class Rocket:
+    """A class to represent a rocket"""
+    def __init__(self, rocket_game) -> None:
+        self.screen = rocket_game.screen
+    
+    def update(self):
+        pass 
+    
+    def blitme(self):
+        pass 
