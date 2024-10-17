dfe99267da4cb773678c7f82485dc976f991c172
ManjotSingh08x <mithugunnu2006@gmail.com>
2024-10-17 14:41:02 +0530

    adding buttons

--- python_crash_course/alien_invasion/alien_invasion.py
+++ python_crash_course/alien_invasion/alien_invasion.py
@@ -1,9 +1,11 @@
 import sys 
+from time import sleep
 import pygame
 from settings import Settings
 from ship import Ship
 from bullet import Bullet
 from alien import Alien
+from game_stats import GameStats
 
 
 
@@ -21,19 +23,29 @@ class AlienInvasion:
             self.settings.screen_height
         ))  
         pygame.display.set_caption("Alien Invasion")
+        
+        # Create an instance to store game statistics
+        self.stats = GameStats(self)
+        
         self.ship = Ship(self)
         self.bullets = pygame.sprite.Group()
         self.aliens = pygame.sprite.Group()
 
         self._create_fleet()
+        
+        # Start Alien Invasion in an inactive state
+        self.game_active = False
     
     def run_game(self):
         """Start the main loop for the game."""
         while True:
             self._check_events()
-            self.ship.update()
-            self._update_bullets() 
-            self._update_aliens()
+            
+            if self.game_active:
+                self.ship.update()
+                self._update_bullets() 
+                self._update_aliens()
+                
             self._update_screen()
             self.clock.tick(100)
     
@@ -96,11 +108,22 @@ class AlienInvasion:
         # Update bullet positions
         self.bullets.update()
         
+        
         # Get rid of old bullets
         for bullet in self.bullets.copy():
             if bullet.rect.bottom <= 0:
                 self.bullets.remove(bullet)
-        #print(len(self.bullets))
+        
+        self._check_bullet_alien_collisions()
+    
+    def _check_bullet_alien_collisions(self):
+        """Respond to bullet-alien collisions."""
+        # Remove any bullets and aliens that have collided
+        collisions = pygame.sprite.groupcollide(self.bullets, self.aliens, True, True)
+        if not self.aliens:
+            # Destroy existing bullets and create new fleet.abs
+            self.bullets.empty()
+            self._create_fleet()
     
     def _create_fleet(self):
         """Create the fleet of aliens."""
@@ -130,7 +153,14 @@ class AlienInvasion:
         """Update the positions of all aliens in the fleet."""
         self._check_fleet_edges()
         self.aliens.update()
-    
+        
+        # Look for alien-ship collisions.
+        if pygame.sprite.spritecollideany(self.ship, self.aliens):
+            self._ship_hit()
+        
+        # Look for aliens hitting the bottom of the screen
+        self._check_aliens_bottom()
+        
     def _check_fleet_edges(self):
         """Respond appropriately if any aliens have reached an edge."""
         for alien in self.aliens.sprites():
@@ -143,6 +173,35 @@ class AlienInvasion:
         for alien in self.aliens.sprites():
             alien.rect.y += self.settings.fleet_drop_speed
         self.settings.fleet_direction *= -1 
+    
+    def _ship_hit(self):
+        """Respond to the ship being hit by an alien."""
+        if self.stats.ships_left > 0:
+            # Decrement ships_left
+            self.stats.ships_left -= 1
+            
+            # Get rid of any remaining bullets and aliens
+            self.bullets.empty()
+            self.aliens.empty()
+            
+            # Create a new fleet and center the Ship
+            self._create_fleet()
+            self.ship.center_ship()
+            print("Ship hit")
+            
+            # Pause
+            sleep(0.5)
+        
+        else:
+            self.game_active = False
+        
+    def _check_aliens_bottom(self):
+        """Check if any aliens have reached the bottom of the screen."""
+        for alien in self.aliens.sprites():
+            if alien.rect.bottom >= self.settings.screen_height:
+                # Treat this the same as if the ship got hit. 
+                self._ship_hit()
+                break
                 
 if __name__ == '__main__':
     #Make a game instance and run the game
